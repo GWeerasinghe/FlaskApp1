85b6412a94c49ea34e415d49e1acaa03a9061c05
gweerasinghe <GWeerasinghe@JacquesSL.com.lk>
2024-12-19 14:18:14 +0530

    Created welcome page and updated other pages

--- login.py
+++ login.py
@@ -1,52 +1,23 @@
-# from flask import Flask, redirect, url_for, request # type: ignore
-
-# app = Flask(__name__)
-
-# # data = []
-
-# @app.route('/success/<name>')
-# def success(name):
-#     return 'Welcome %s!' % name
-
-# @app.route('/login', methods=['POST','GET'])
-# def login():
-#     if request.method == 'POST':
-#         name = request.form.get['name']
-#         # age = request.form.get['age']
-#         # data.append({'name': name, 'age': age})
-#         return redirect(url_for('success', name=name))
-#     else:
-#         user = request.args.get('nm')
-#         return redirect(url_for('success', name=name))
-#     # return render_template('login.html', data=data)
-
-# # @app.route('/success/<name>/<age>')
-# # def success(name,age):
-# #         name = request.args.get('name')
-# #         age = request.args.get('age', default=None, type=int)
-# #         return f'Welcome, {name}! You are {age} years old.'
-    
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-from flask import Flask, redirect, url_for, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/success/<name>')
-def success(name):
-    return 'Welcome %s!' % name
-
+@app.route('/')
+def welcome():
+    return render_template("login.html")
+database = {'Gayani':'1234', 'James':'5678', 'Nuwani':'0123'}
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))
+    name = request.form['nm']
+    password = request.form['pw']
+    if name not in database:
+        return render_template('login.html', info='Invalid User..')
     else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name=user))
+        if database[name]!=password:
+            return render_template('login.html', info='Invalid Password.Try Again.')
+        else:
+            return render_template('home.html', name=name)
 
 
 if __name__ == '__main__':
